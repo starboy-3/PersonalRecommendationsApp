d3fcc47530de2d2b98fab0d0178ed646e10135ba
akosimov <akosimov@yandex-team.ru>
2022-03-08 21:42:36 +0300

    Add pipelines

---
+++ app/app/pipelines/CheckFieldsPresentPipeline.py
@@ -0,0 +1,14 @@
+
+
+class CheckFieldPresentPipeline:
+    def __init__(self):
+        pass
+
+    def open_spider(self, spider):
+        pass
+
+    def process_item(self, item, spider):
+        pass
+
+    def close_spider(self, spider):
+        pass
---
+++ app/app/pipelines/PostgreSQLStorePipeline.py
@@ -0,0 +1,26 @@
+import psycopg2
+from scrapy.exporters import CsvItemExporter
+from dynaconf import settings
+
+
+class PostgreSQLStorePipeline:
+    def __init__(self):
+        self.cur = None
+        self.connection = None
+
+    def open_spider(self, spider):
+        hostname = settings["db_hostname"]
+        username = settings['db_username']
+        password = settings['db_password']
+        database = settings["db_name"]
+        self.connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+        self.cur = self.connection.cursor()
+
+    def process_item(self, item, spider):
+        self.cur.execute("insert into quotes_content(content,author) values(%s,%s)", (item['content'], item['author']))
+        self.connection.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.connection.close()
