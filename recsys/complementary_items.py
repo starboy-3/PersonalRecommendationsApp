261e91642542d2f0f24f41fcd2ec0389d9f2f0c8
Saidakbar Gaziev <si.gaziev@gmail.com>
2022-05-07 22:41:03 +0300

    bugix; collaborative-based systems' faster methods to recommend were added

--- recsys/complementary_items.py
+++ recsys/complementary_items.py
@@ -15,20 +15,34 @@ class ComplementItemRS(structural.CollaborativeFiltering):
     """
 
     def __init__(self):
-        """
-           TODO для скорости выдачи рекомендации юзеру можем использовать матричную факторизацию QR/SVD/RSVD
-        """
         super(ComplementItemRS, self).__init__()
         # items similarities matrix (items x items)
         self.similarities = None
+        # items similarities matrix QR-decomposition matrices
+        self.sim_q, self.sim_r = None, None
+        # fast-recommendations matrix
+        self.recommendations = None
+        # use to save memory resources from lots of data possibly
+        # never been recommended
+        self.fast_reqs_limit = -1
 
-    def build(self, via_normalize: bool = False):
+    def set_limit(self, value: int = 100):
+        """
+        :param value: new value to be set
+        """
+        self.fast_reqs_limit = value
+
+    def build(self,
+              via_normalize: bool = False,
+              big_data: bool = False):
         """
         :param via_normalize: set true if want to normalize items'
             vectors. They can be normalized for the situations, where
             opinion of man is more valuable if he has expressed it
             less compared to others (briefly, in binary data it must
             be nice).
+        :param big_data: set true if want to multiply QR-decomposition
+            for items' similarities matrix (only for big data's)
         """
         if via_normalize:
             self.sparse_matrix = self.sparse_matrix.astype(np.float32)
@@ -44,6 +58,23 @@ class ComplementItemRS(structural.CollaborativeFiltering):
                 )
         # count cosine similarity matrix
         self.similarities = cosine_similarity(self.sparse_matrix.transpose())
+
+        if big_data:
+            self.sim_q, self.sim_r = np.linalg.qr(self.similarities)
+
+            # counting how all users relates to all items
+            self.recommendations = (self.sim_q @ (self.sim_r @ self.sparse_matrix.T))
+        else:
+            self.recommendations = (self.similarities @ self.sparse_matrix.T)
+        # compared to max l1-norm of exact item's vector
+        self.recommendations /= np.array([np.abs(self.similarities).sum(axis=1)]).T
+
+        # remove from our answer set already liked by users items
+        for user_id in range(self.sparse_matrix.shape[0]):
+            for i in range(self.sparse_matrix.indptr[user_id],
+                           self.sparse_matrix.indptr[user_id + 1]):
+                self.recommendations[self.sparse_matrix.indices[i], user_id] = 0
+        self.recommendations = np.argsort(-self.recommendations.T, axis=1)[:, :self.fast_reqs_limit]
 
     def find_similar_item(self,
                           item,
@@ -63,13 +94,19 @@ class ComplementItemRS(structural.CollaborativeFiltering):
     def recommend_to_user(self,
                           user,
                           res_n: int = 10):
-        """ если хотим recommend for this order, то user (здесь это order)
-            должен быть пустым; для лучше работы нужен метод update или тп
+        """
+        :param user: user as it is stored in database
+            (meant it is in column that was given to load)
+        :param res_n: count of items to recommend
+        :return: `res_n` most suitable (by RS) items for `user`
         """
         user_id = self.user2index(user)
 
         # counting how our user relates to all items
-        scores_vec = (self.similarities @ self.sparse_matrix[user_id, :].T)
+        if self.sim_q is None:
+            scores_vec = (self.similarities @ self.sparse_matrix[user_id, :].T)
+        else:
+            scores_vec = (self.sim_q @ (self.sim_r @ self.sparse_matrix[user_id, :].T))
         # compared to max l1-norm of exact item's vector
         scores_vec /= np.array([np.abs(self.similarities).sum(axis=1)]).T
 
@@ -81,6 +118,25 @@ class ComplementItemRS(structural.CollaborativeFiltering):
         # choose top `res_n` and return them
         return self.index2item(np.argsort(scores_vec.T)[0][::-1][:res_n])
 
+    def fast_recommend(self,
+                       user,
+                       res_n: int = 10):
+        """
+        :param user: user as it is stored in database
+            (meant it is in column that was given to load)
+        :param res_n: count of items to recommend
+        :return: `res_n` most suitable (by RS) items for `user`
+        """
+        user_id = self.user2index(user)
+        rec_vector = self.recommendations[user_id, :]
+        return self.index2item(rec_vector[:res_n])
+
+    def drop_slow(self):
+        del self.similarities
+        del self.sim_r
+        del self.sim_q
+        del self.sparse_matrix
+
 
 if __name__ == '__main__':
     complement_items = ComplementItemRS()
@@ -88,7 +144,7 @@ if __name__ == '__main__':
     print("loading started...")
     complement_items.load("small_data/lastfm2collab.csv", ["user_id", "item_id"])
     print("loading finished...")
-
+    complement_items.set_limit()
     print("building started...")
     complement_items.build()
     print("building finished...")
@@ -100,3 +156,9 @@ if __name__ == '__main__':
     q_user = 5985
     user_recs = complement_items.recommend_to_user(q_user)
     print("\nusers recs:", *user_recs, sep="\n")
+
+    complement_items.drop_slow()
+    user_fast_recs = complement_items.fast_recommend(q_user)
+    print("\nusers fast recs:", *user_fast_recs, sep="\n")
+
+    print("\ninter:", *(set(user_recs) & set(user_fast_recs)), sep="\n")
